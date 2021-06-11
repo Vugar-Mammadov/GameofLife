9ff6c381e7c76cbd1a8e209dfcd97837d44d1c3f
vuqarm547 <vuqarm547@gmail.com>
2021-06-12 02:57:12 +0400

    updated: cmake

---
+++ build/game/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/life.c.o"
+  "CMakeFiles/game.dir/life.c.o.d"
+  "libgame.pdb"
+  "libgame.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
